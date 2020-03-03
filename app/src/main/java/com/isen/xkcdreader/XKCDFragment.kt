6019d4c277e7e60ea9fe018b71005971139b2936
remicmacs <rembourgeon@gmail.com>
2020-03-03 18:33:20 +0100

    Replace ImageView by TouchImageView

--- app/src/main/java/com/isen/xkcdreader/XKCDFragment.kt
+++ app/src/main/java/com/isen/xkcdreader/XKCDFragment.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.ortiz.touchview.TouchImageView
 
 class XKCDFragment: Fragment() {
     private lateinit var title : String
@@ -34,7 +34,7 @@ class XKCDFragment: Fragment() {
         val view =  inflater.inflate(R.layout.xkcd_fragment, container, false)
         val title : TextView = view.findViewById(R.id.xkcd_title)
         val altText : TextView = view.findViewById(R.id.xkcd_alt_text)
-        val img : ImageView = view.findViewById(R.id.xkcd_img)
+        val img : TouchImageView = view.findViewById(R.id.xkcd_img)
         title.text = this.title
         altText.text = this.altText
         img.setImageBitmap(this.img)
