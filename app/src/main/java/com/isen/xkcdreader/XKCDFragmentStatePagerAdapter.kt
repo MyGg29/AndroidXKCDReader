b87633317c89d31012b42852604c17504b51481a
rodolpheh <houdas.rodolphe@gmail.com>
2020-03-03 14:58:51 +0100

    Adding random button feature

--- app/src/main/java/com/isen/xkcdreader/XKCDFragmentStatePagerAdapter.kt
+++ app/src/main/java/com/isen/xkcdreader/XKCDFragmentStatePagerAdapter.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 
 class XKCDFragmentStatePagerAdapter(val fragmentManager: FragmentManager, val xkcds : ArrayList<XKCDItem>)
-    : FragmentStatePagerAdapter(fragmentManager) {
+    : FragmentStatePagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
     override fun getItem(position: Int): Fragment {
         return XKCDFragment.newInstance(xkcds[position])
     }
