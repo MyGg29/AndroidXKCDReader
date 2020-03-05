a850da7233fb14ab346a52b22ca493a49e3dfae6
rodolpheh <houdas.rodolphe@gmail.com>
2020-03-05 16:13:18 +0100

    Adding animation to splash screen

--- app/src/main/java/com/isen/xkcdreader/SplashActivity.kt
+++ app/src/main/java/com/isen/xkcdreader/SplashActivity.kt
@@ -2,8 +2,18 @@ package com.isen.xkcdreader
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import android.widget.ImageView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.ImageRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import java.net.URL
 
 
 class SplashActivity : Activity() {
@@ -16,13 +26,29 @@ class SplashActivity : Activity() {
         setContentView(R.layout.splash_layout)
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
-        Handler()
-            .postDelayed({
-                // This method will be executed once the timer is over
-                // Start your app main activity
+
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://xkcd.com/info.0.json"
+
+        // Request a string response from the provided URL.
+        val jsonRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+
+            Response.Listener { response ->
+
+                // Display the response and save it temporarily
+                Log.d("Pulling data", "Response is: $response")
 
                 startActivity(Intent(this, MainActivity::class.java))
                 finish()
-            }, SPLASH_DISPLAY_LENGTH)
+            },
+
+            Response.ErrorListener {
+                Log.e("Volley", "Error while waiting for response", it)
+            }
+        )
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonRequest)
     }
 }
